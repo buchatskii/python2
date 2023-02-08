220cdf98fd2cc95247b4c33551aee071bb1a346f
buchatskii <103274724+buchatskii@users.noreply.github.com>
2023-02-08 17:29:59 +0300

    Update dz3.py
    Обновил решение 20 задачи!

--- dz3.py
+++ dz3.py
@@ -6,31 +6,39 @@
 # print(spam.count(num))
 
 # Задача 18
-number = int(input("Введите размер списка: "))
-num = int(input("Ближайшие к числу: "))
-spam = list(range(1, number + 1))
-for i in spam:
-    if i == num:
-        print(num - 1, num + 1)
+# number = int(input("Введите размер списка: "))
+# num = int(input("Ближайшие к числу: "))
+# spam = list(range(1, number + 1))
+# for i in spam:
+#     if i == num:
+#         print(num - 1, num + 1)
 
 
 
 
 # Задача 20
-# list_1 = {
-#     1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R'],
-#     2: ['D', 'G'],
-#     3: ['B', 'C', 'M', 'P'],
-#     4: "F, H, V, W, Y",
-#     5: "K"],
-#     8: "J, X"],
-#     10: "Q, Z"]
-# }
-# point = 0
-# word = str.upper(input("Введите слово: "))
-# for i in word:
-#     for j in list_1:
-#         if i in list_1[j]:
-#             point += 1
-# print(point)
+list_1 = {
+    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R'],
+    2: ['D', 'G'],
+    3: ['B', 'C', 'M', 'P'],
+    4: ['F', 'H', 'V', 'W', 'Y'],
+    5: ['K'],
+    8: ['J', 'X'],
+    10: ['Q', 'Z'],
+    1: ['А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т'],
+    2: ['Д', 'К', 'Л', 'М', 'П', 'У'],
+    3: ['Б', 'Г', 'Ё', 'Ь', 'Я'],
+    4: ['Й', 'Ы'],
+    5: ['Ж', 'З', 'Х', 'Ц', 'Ч'],
+    8: ['Ш', 'Э', 'Ю'],
+    10: ['Ф', 'Щ', 'Ъ'],
+}
+point = 0
+word = str.upper(input("Введите слово: "))
+for i in word:
+    for j in list_1:
+        if i in list_1[j]:
+            point += j
+            continue
+print(point)
 
