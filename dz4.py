7d5f8f67a78fa4ff8ef081a56e0f9f8fd11c8e6c
buchatskii <103274724+buchatskii@users.noreply.github.com>
2023-02-10 15:08:16 +0300

    Выполнил дз 4

--- dz4.py
+++ dz4.py
@@ -1,3 +1,4 @@
+# Задача 22
 import random
 num_1 = 6
 num_2 = 10
@@ -17,3 +18,13 @@ random_list_2.sort()
 print(random_list_2)
 c = set(random_list_1) & set(random_list_2)
 print(sorted(c))
+
+# Задача 24
+# number = int(input("Введите размер списка: "))
+# spam = list(range(1, number + 1))
+# print(spam)
+# pid = 0
+# for i in spam:
+#     if i == number:
+#         pid += (i - 1) + spam[0] + i
+#       print(pid)
